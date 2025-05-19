45f4d26e761fa85470839735bd7701e1b6e74b82
Huang Chuan <river1440>
2025-05-19 12:09:07 +0800

    feat: from kaggle to mysql container

---
+++ define_keys.sql
@@ -0,0 +1,42 @@
+-- Customers table
+ALTER TABLE customers
+  ADD PRIMARY KEY (customer_id);
+
+-- Sellers table
+ALTER TABLE sellers
+  ADD PRIMARY KEY (seller_id);
+
+-- Geolocation
+ALTER TABLE geolocation
+  ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY;
+
+-- Product Category Translation table
+ALTER TABLE product_category_name_translation
+  ADD PRIMARY KEY (product_category_name);
+
+-- Products table
+ALTER TABLE products
+  ADD PRIMARY KEY (product_id),
+  ADD FOREIGN KEY (product_category_name) REFERENCES product_category_name_translation(product_category_name);
+
+-- Orders table
+ALTER TABLE orders
+  ADD PRIMARY KEY (order_id),
+  ADD FOREIGN KEY (customer_id) REFERENCES customers(customer_id);
+
+-- Order Items table
+ALTER TABLE order_items
+  ADD PRIMARY KEY (order_id, order_item_id),
+  ADD FOREIGN KEY (order_id) REFERENCES orders(order_id),
+  ADD FOREIGN KEY (product_id) REFERENCES products(product_id),
+  ADD FOREIGN KEY (seller_id) REFERENCES sellers(seller_id);
+
+-- Order Payments table
+ALTER TABLE order_payments
+  ADD PRIMARY KEY (order_id, payment_sequential),
+  ADD FOREIGN KEY (order_id) REFERENCES orders(order_id);
+
+-- Order Reviews table
+ALTER TABLE order_reviews
+  ADD PRIMARY KEY (review_id, order_id),
+  ADD FOREIGN KEY (order_id) REFERENCES orders(order_id);
