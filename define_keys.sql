befd5f5451579d51b516d7335e20b6f4fc909d35
Huang Chuan <river1440>
2025-05-19 14:49:45 +0800

    fix: rename pk

--- define_keys.sql
+++ define_keys.sql
@@ -1,10 +1,10 @@
 -- Customers table
 ALTER TABLE customers
-  ADD PRIMARY KEY (customer_id);
+  ADD PRIMARY KEY (id);
 
 -- Sellers table
 ALTER TABLE sellers
-  ADD PRIMARY KEY (seller_id);
+  ADD PRIMARY KEY (id);
 
 -- Geolocation
 ALTER TABLE geolocation
@@ -16,27 +16,27 @@ ALTER TABLE product_category_name_translation
 
 -- Products table
 ALTER TABLE products
-  ADD PRIMARY KEY (product_id),
+  ADD PRIMARY KEY (id),
   ADD FOREIGN KEY (product_category_name) REFERENCES product_category_name_translation(product_category_name);
 
 -- Orders table
 ALTER TABLE orders
-  ADD PRIMARY KEY (order_id),
-  ADD FOREIGN KEY (customer_id) REFERENCES customers(customer_id);
+  ADD PRIMARY KEY (id),
+  ADD FOREIGN KEY (customer_id) REFERENCES customers(id);
 
 -- Order Items table
 ALTER TABLE order_items
   ADD PRIMARY KEY (order_id, order_item_id),
-  ADD FOREIGN KEY (order_id) REFERENCES orders(order_id),
-  ADD FOREIGN KEY (product_id) REFERENCES products(product_id),
-  ADD FOREIGN KEY (seller_id) REFERENCES sellers(seller_id);
+  ADD FOREIGN KEY (order_id) REFERENCES orders(id),
+  ADD FOREIGN KEY (product_id) REFERENCES products(id),
+  ADD FOREIGN KEY (seller_id) REFERENCES sellers(id);
 
 -- Order Payments table
 ALTER TABLE order_payments
   ADD PRIMARY KEY (order_id, payment_sequential),
-  ADD FOREIGN KEY (order_id) REFERENCES orders(order_id);
+  ADD FOREIGN KEY (order_id) REFERENCES orders(id);
 
 -- Order Reviews table
 ALTER TABLE order_reviews
   ADD PRIMARY KEY (review_id, order_id),
-  ADD FOREIGN KEY (order_id) REFERENCES orders(order_id);
+  ADD FOREIGN KEY (order_id) REFERENCES orders(id);
